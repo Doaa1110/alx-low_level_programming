063ac439ff13318eab3bb6a473c2078ec0d77d42
Doaa1110 <doaaidris7@gmail.com>
2023-08-10 18:37:45 -0700

    C Task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Description: A C program using sizeof function
+ *
+ * Return: Always0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char:", sizeof(char));
+	printf("Size of an int:", sizeof(int));
+	printf("Size of a long int:", sizeof(long int));
+	printf("Size of a long long int:", sizeof(long long int));
+	printf("Size of a float:", sizeof(float));
+	return (0);
+}
