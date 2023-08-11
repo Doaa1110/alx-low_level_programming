7ccd9c6bc0ce232063bac8e32287fc5cbe37a2a5
Doaa1110 <doaaidris7@gmail.com>
2023-08-10 19:26:24 -0700

    C Task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: A C program using printf function
+ *
+ * Return: Always 0 (Success)
+*/
+void myFunction(void)
+{
+	printf("and that piece of art is useful"" - Dora Korpar, 2015-10-19\n");
+}
+int main(void)
+
+{
+	myFunction();
+return (1);
+}
