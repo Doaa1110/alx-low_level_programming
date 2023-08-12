dd4c61b1de2c7480d2f8168dbd7988db88a859de
Doaa1110 <doaaidris7@gmail.com>
2023-08-11 19:23:14 -0700

    C Task 2

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program using print lphabet in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar("\n");
+
+	return (0);
+}
