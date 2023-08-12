b768b7159a16458c116da3b0acb6521f7768f753
Doaa1110 <doaaidris7@gmail.com>
2023-08-11 18:58:19 -0700

    C Task 2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program using printf singil digit number function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digitN = 0;
+
+	while (digitN <= 9)
+	{
+		printf("%i". digitN);
+		digitN++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
