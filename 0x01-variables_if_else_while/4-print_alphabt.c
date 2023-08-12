669c0d23d53c9bfe4b0a951c6f388ec5a9fa8eb3
Doaa1110 <doaaidris7@gmail.com>
2023-08-11 18:42:37 -0700

    C Task 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program using putchar function
+ *
+ * Return: Always0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+			c++
+				putchar(c);
+		c++
+	}
+	putcahr('\n');
+
+	return (0);
+}
