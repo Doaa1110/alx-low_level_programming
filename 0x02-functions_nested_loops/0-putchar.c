b0afdba1fa4f7164c5eb9cc8b78fdbeeab72dcc7
Doaa1110 <doaaidris7@gmail.com>
2023-08-15 06:47:26 -0700

    c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include  "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: A C program using _putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int _putchar(char c)
+		_putchar('%n');
+	return (0);
+}
