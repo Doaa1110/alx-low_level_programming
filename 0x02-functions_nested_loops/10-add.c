b1add7c1b9d71c13762ee6679cbbaa54880f0603
Doaa1110 <doaaidris7@gmail.com>
2023-08-16 16:21:18 -0700

    C nested

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,16 +4,15 @@
 /**
  * add - function that adds two integers and returns the result
  *
- * @n1 - fisrt number
- * @n2 - second number
+ * @n1: fisrt number
+ * @n2: second number
  *
- * Return: Always 0.
+ * Return: Always result
  */
 
 int add(int n1, int n2)
 {
 	int result;
 result = n1 + n2;
-printf("%i\n", result);
-return (0);
+return (result);
 }
