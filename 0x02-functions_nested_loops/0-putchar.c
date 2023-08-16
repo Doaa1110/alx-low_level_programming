14c0ea2be752b37a85ddc2085e5d9239620675fc
Doaa1110 <doaaidris7@gmail.com>
2023-08-15 17:44:27 -0700

    C nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
-#include <stdio.h>
-#include  "main.h"
+#include "main.h"
 
 /**
- * main - entry point
- *
- * Description: A C program using _putchar
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by fnl
+ * Return: 0
 */
 
 int main(void)
 {
-	int _putchar(char c)
-		_putchar('%n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
