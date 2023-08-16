46cb6bca5ccb67b7a77b3ae6271911d002ff71df
Doaa1110 <doaaidris7@gmail.com>
2023-08-15 19:42:04 -0700

    C nested

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int minute, h;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h / 10) + 48);
+			_putchar((':');
+			_putchar((minute / 10) + 48);
+			_putchar((minute % 10) + 48);
+			_putchar(('\n');
+				}
+				}
+				}
