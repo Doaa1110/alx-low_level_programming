d27afa07c3990240758ab8f526a74fcf28c0f295
Doaa1110 <doaaidris7@gmail.com>
2023-08-22 12:09:12 -0700

    Pointer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that  prints a string, followed by a new line
+ *
+ *@s: char  pointer
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int a, i;
+	char t;
+
+	for (a = 0; s[a] != '\0'; ++a)
+
+		for (i = 0; i < a / 2; i++)
+		{
+			t = s[i];
+			s[i] = s[a - 1 - i];
+			s[a - 1 - i] = temp;
+		}
+}
