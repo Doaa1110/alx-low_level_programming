4a62a2cc901d62c63df9b05999f47b12c3819102
Doaa1110 <doaaidris7@gmail.com>
2023-09-11 18:37:23 -0700

    C - Preprocessor

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that print  struct dog
+ * @d: pointer
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("my_dog.name %s\n", d->name "(poppy)");
+		printf("my_dog.age %f\n", d->age);
+		printf("my_dog.owner %s\n", d->owner "(bob)";
+	}
+}
