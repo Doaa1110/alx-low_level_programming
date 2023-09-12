76b465c637ce75270cfeed69d0f26772283d81fd
Doaa1110 <doaaidris7@gmail.com>
2023-09-11 19:55:27 -0700

    C - Preprocessor

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,18 +3,62 @@
 #include <stdlib.h>
 
 /**
+ * _len - function that for string length
+ *
+ * @i: string
+ *
+ * Return: length
+ */
+int _len(char *i)
+{
+	int j = 0;
+
+	while (i[j] != '\0')
+	{
+		j++;
+	}
+	return (j);
+}
+
+/**
+ * _copy - function that copy string
+ *
+ * @i: string
+ * @j: string
+ *
+ * Return: j
+ */
+char _copy(char *j, char *i)
+{
+	int k;
+
+	for (k = 0; i[k]; k++)
+		j[k] = i[k];
+	j[k] = '\0';
+
+	return (j);
+}
+
+/**
  * print_dog - function that print  struct dog
- * @d: pointer
+ *
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
  *
  * Return: void
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (!name || age < 0 || !owner)
+		return (NULL);
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	return (NULL);
+	dog->name = malloc(sizeof(char) * (_len(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 }
